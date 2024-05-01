9c871540c7865aebfdd8913fe5ac62de8ab40cc1
xinpingye <srtyfung@qq.com>
2024-05-01 23:52:12 +0800

    ps2 p2-1 complete Edge ADT design, test, implement and ConcreteEdgesGraph implement , but test fail, for future debug

--- src/graph/ConcreteEdgesGraph.java
+++ src/graph/ConcreteEdgesGraph.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.HashMap;
 
 /**
  * An implementation of Graph.
@@ -20,46 +21,225 @@ public class ConcreteEdgesGraph implements Graph<String> {
     private final List<Edge> edges = new ArrayList<>();
     
     // Abstraction function:
-    //   TODO
+    // represent a graph , all vertex is in vertices and all edges is in edges,edge is directed and weighted
     // Representation invariant:
-    //   TODO
+    // all vertex associated with edges is in vertices, and all weight associated with edges is bigger than zero, and edges is not duplicated 
     // Safety from rep exposure:
-    //   TODO
+    // all fields are private,  vertices is mutable, so ConcreteEdgesGraph() and vertices() make defensive copies to avoid sharing class members to clients 
     
     // TODO constructor
+    /**
+     * same as default(because data fields have member construct)
+     */
+    public ConcreteEdgesGraph()
+    {}
     
     // TODO checkRep
+    /**
+     * check representation invariant is not exposure 
+     */
+    private void checkRep()
+    {
+        for(Edge e : edges)
+        {
+            assert vertices.contains(e.getSource());
+            assert vertices.contains(e.getTarget());
+            assert e.getWeight() > 0;
+        }
+        
+        for(int i = 0; i < edges.size(); i++)
+        {
+            for(int j = i + 1; j < edges.size(); j++)
+            {
+                assert !Edge.compare(edges.get(i), edges.get(j));
+            }
+        }
+    }
     
     @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        if(vertices.contains("vertex"))
+        {
+            checkRep();
+            
+            return false;
+        }
+        else
+        {
+            vertices.add(vertex);
+            
+            checkRep();
+            
+            return true;
+        }
     }
     
     @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        
+        int exist_flag = 0;
+        
+        for(Edge e : edges)
+        {
+            if(e.getSource().equals(source) && e.getTarget().equals(target))
+            {
+                exist_flag = 1;
+            }
+        }
+        
+        if(exist_flag == 0)
+        {
+            if(weight == 0)
+            {
+                checkRep();
+                
+                return 0;
+            }
+            else
+            {
+                vertices.add(source);
+                vertices.add(target);
+                
+                Edge e = new Edge(source, target, weight);
+                
+                edges.add(e);
+                
+                checkRep();
+                
+                return 0;
+            }
+        }
+        else
+        {
+            if(weight == 0)
+            {
+                for(Edge e : edges)
+                {
+                    if(e.getSource().equals(source) && e.getTarget().equals(target))
+                    {
+                        Integer return_weight = e.getWeight();
+                        
+                        edges.remove(e);
+                       
+                        checkRep();
+                        
+                        return return_weight.intValue();
+                    }
+                }
+            }
+            else
+            {
+                for(Edge e : edges)
+                {
+                    if(e.getSource().equals(source) && e.getTarget().equals(target))
+                    {   
+                        Integer return_weight = e.getWeight();
+                        
+                        Edge insert_e = new Edge(e.getSource(), e.getTarget(), weight);
+                        
+                        edges.remove(e);
+                        
+                        edges.add(insert_e);
+                        
+                        checkRep();
+                        
+                        return return_weight.intValue();
+                    }
+                }  
+            }
+        }
+        
+        return -1;
+
     }
     
     @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        
+        if(vertices.contains(vertex))
+        {
+            vertices.remove(vertex);
+            
+            List<Edge> remove_list = new ArrayList<>(); 
+            
+            for(Edge e : edges)
+            {
+                if(e.getSource().equals(vertex) || e.getTarget().equals(vertex))
+                    remove_list.add(e);
+            }
+            
+            edges.removeAll(remove_list);
+            
+            checkRep();
+            
+            return true;
+        }
+        else
+        {
+            checkRep();
+            
+            return false;
+        }
     }
     
     @Override public Set<String> vertices() {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        
+        Set<String> return_vertices_set = new HashSet<>();
+        
+        for(String s : vertices)
+        {
+            return_vertices_set.add(s);
+        }
+        
+        checkRep();
+        
+        return return_vertices_set;
     }
     
     @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        
+        Map<String, Integer> return_source_map = new HashMap<>();
+        
+        for(Edge e : edges)
+        {
+            if(e.getTarget().equals(target))
+                return_source_map.put(e.getSource(), e.getWeight());
+        }
+        
+        checkRep();
+        
+        return return_source_map;
     }
     
     @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        
+        Map<String, Integer> return_target_map = new HashMap<>();
+        
+        for(Edge e : edges)
+        {
+            if(e.getSource().equals(source))
+                return_target_map.put(e.getTarget(), e.getWeight());
+        }
+        
+        checkRep();
+        
+        return return_target_map;
+        
     }
     
     // TODO toString()
     
+    @Override public String toString()
+    {
+        return "test";
+    }
 }
 
 /**
- * TODO specification
+ * this immutable data type represent a Edge
  * Immutable.
  * This class is internal to the rep of ConcreteEdgesGraph.
  * 
@@ -67,22 +247,83 @@ public class ConcreteEdgesGraph implements Graph<String> {
  * up to you.
  */
 class Edge {
-    
-    // TODO fields
-    
+    //fields
+    private final String  source;
+    private final String  target;
+    private final Integer weight;
     // Abstraction function:
-    //   TODO
+    // represent a edge which source point to target vertex with the same weight
     // Representation invariant:
-    //   TODO
+    // weight > 0
     // Safety from rep exposure:
-    //   TODO
+    // all fields are private and all types in the rep are immutable
     
-    // TODO constructor
+    // creator method
+    /**
+     * make a new Edge
+     * @param source    the source vertex of the Edge
+     * @param target    the target vertex of the Edge
+     * @param weight    the weight of the Edge
+     */
+    public Edge(String source, String target, Integer weight)
+    {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
+        
+        checkRep();
+    }
+    // checkRep
+    /**
+     * check  representation invariant of the Edge is true 
+     */
+    private void checkRep()
+    {
+        assert source != null;
+        assert target != null;
+        assert weight != null;
+       
+        assert weight > 0;
+    }
+    // observer methods
+    /**
+     * @return return source vertex of this Edge
+     */
+    public String getSource()
+    {
+        return source;
+    }
     
-    // TODO checkRep
+    /**
+     * @return return target vertex of this Edge
+     */
+    public String getTarget()
+    {
+        return target;
+    }
     
-    // TODO methods
+    /**
+     * @return return weight of this Edge
+     */
+    public Integer getWeight()
+    {
+        return weight;
+    }
+    // toString()
+    @Override public String toString()
+    {
+        return source + "-" + weight + "->" + target;
+    }
     
-    // TODO toString()
+    /**
+     * compare ADT object e1 and e2 
+     * @param e1 
+     * @param e2
+     * @return if e1 equal with e2 , return true, else return false
+     */
+    public static boolean compare(Edge e1, Edge e2)
+    {
+        return (e1.getSource() == e2.getSource() && e1.getTarget() ==e2.getTarget());
+    }
     
 }
--- test/graph/ConcreteEdgesGraphTest.java
+++ test/graph/ConcreteEdgesGraphTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+//import graph.Edge;
+
 /**
  * Tests for ConcreteEdgesGraph.
  * 
@@ -38,8 +40,38 @@ public class ConcreteEdgesGraphTest extends GraphInstanceTest {
      */
     
     // Testing strategy for Edge
-    //   TODO
+    // Edge():
+    // weight <= 0, weight > 0
+    // getSource():
+    // edge create by Edge()
+    // getTarget():
+    // edge create by Edge()
+    // getWeight():
+    // edge create by Edge()
+    // toString():
+    // edge create by Edge()
+    // compare()
+    // e1 same with e2 or e1 not same with e2
     
     // TODO tests for operations of Edge
-    
+    @Test
+    public void testEdge()
+    {
+        Edge e = new Edge("Num1", "Num2", Integer.valueOf(1));
+        
+        assertEquals("expected source is Num1", "Num1", e.getSource());
+        assertEquals("expected target is Num2", "Num2", e.getTarget());
+        assertEquals("expected weight is 1", Integer.valueOf(1), e.getWeight());
+        
+        assertEquals("expected correct toString output", "Num1-1->Num2", e.toString());
+        
+        Edge e1 = new Edge("Num1", "Num3", Integer.valueOf(1));
+        
+        Edge e2 = new Edge("Num1", "Num3", Integer.valueOf(1));
+        
+        assertFalse("expect e not same with e1", Edge.compare(e, e1));
+        assertTrue("expect e1 is same with e2", Edge.compare(e1, e2));
+        
+        // not to test weight <= 0 case
+    }
 }
