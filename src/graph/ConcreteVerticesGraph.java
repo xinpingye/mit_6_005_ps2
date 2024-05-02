659408ef3428bb47ec2d7cae2e85fd64717c9ec2
xinpingye <srtyfung@qq.com>
2024-05-02 15:50:15 +0800

    ps2-p2 complete and test success

--- src/graph/ConcreteVerticesGraph.java
+++ src/graph/ConcreteVerticesGraph.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * An implementation of Graph.
@@ -18,38 +20,286 @@ public class ConcreteVerticesGraph implements Graph<String> {
     private final List<Vertex> vertices = new ArrayList<>();
     
     // Abstraction function:
-    //   TODO
+    // represent a graph and all vertex is in vertices and all edge also in vertices
     // Representation invariant:
-    //   TODO
+    // all vertices in graph in this vertices and all edges is also in vertices.all weight associated with edges is bigger than zero, and Vertex is not duplicated 
     // Safety from rep exposure:
-    //   TODO
+    // all fields is private and make defensive for targets()
     
-    // TODO constructor
-    
+    // constructor
+    /**
+     * same as default(because data fields have member construct)
+     */
+    public ConcreteVerticesGraph()
+    {
+        
+    }
     // TODO checkRep
+    private void checkRep()
+    {
+        assert vertices != null;
+        
+        List<String> vertices_lable_vertex = new ArrayList<>();
+        List<String> vertices_lable_vertex_target = new ArrayList<>();
+        
+        for(Vertex v : vertices)
+        {
+            vertices_lable_vertex.add(v.getVertex());
+            vertices_lable_vertex_target.addAll(v.getEdge().keySet());
+        }
+        
+        for(String s : vertices_lable_vertex_target)
+        {
+            assert vertices_lable_vertex.contains(s);
+        }
+        
+        for(int i = 0; i < vertices_lable_vertex.size(); i++)
+        {
+            for(int j = i + 1; j < vertices_lable_vertex.size(); j++)
+            {
+                assert !vertices_lable_vertex.get(i).equals(vertices_lable_vertex.get(j));
+            }
+        }
+    }
     
     @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        
+        int exist_flag = 0;
+        
+        for(Vertex v : vertices)
+        {
+            if(v.getVertex().equals(vertex))
+            {
+                exist_flag = 1;
+                break;
+            }
+        }
+        
+        if(exist_flag == 1)
+                return false;
+        
+        Vertex v = new Vertex(vertex);
+        
+        vertices.add(v);
+        
+        checkRep();
+        
+        return true;
+        
     }
     
     @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        
+        int exist_flag_source = 0;
+        int exist_flag_target = 0;
+        
+        Vertex exist_vertex_source = null;
+        Vertex exist_vertex_target = null;
+        
+        for(Vertex v : vertices)
+        {
+            if(v.getVertex().equals(source))
+            {
+                exist_flag_source = 1;
+                
+                exist_vertex_source = v;
+            }
+            
+            if(v.getVertex().equals(target))
+            {
+                exist_flag_target = 1;
+                
+                exist_vertex_target = v;
+            }
+        }
+        
+        if(exist_flag_source == 0)
+        {
+            if(weight > 0)
+            {
+                Vertex v = new Vertex(source);
+                
+                v.add_edge(target, weight);
+                
+                vertices.add(v);
+                
+                if(exist_flag_target == 0)
+                {
+                    Vertex e = new Vertex(target);
+                    
+                    vertices.add(e);
+                }
+                
+                checkRep();
+                
+                return 0;
+            }
+            else if(weight == 0)
+            {                
+                checkRep();
+                
+                return 0;
+            }
+        }
+        else
+        {
+            Map<String, Integer> edges = exist_vertex_source.getEdge();
+            
+            int edge_exist_flag = 0;
+            
+            int edge_exist_weight = -1;
+            
+            for(Map.Entry<String, Integer> entry : edges.entrySet())
+            {
+                if(entry.getKey().equals(target))
+                {
+                    edge_exist_flag = 1;
+                    
+                    edge_exist_weight = entry.getValue();
+                    
+                    break;
+                }
+            }
+            
+            if(weight > 0)
+            {
+                if(exist_flag_target == 0)
+                {
+                    Vertex e = new Vertex(target);
+                    
+                    vertices.add(e);
+                }  
+                
+                if(edge_exist_flag == 1)
+                {
+                    exist_vertex_source.set_edge(target, weight);
+                    
+                    checkRep();
+                    
+                    return edge_exist_weight;
+                }
+                else 
+                {
+                    exist_vertex_source.add_edge(target, weight);
+                    
+                    checkRep();
+                    
+                    return 0;
+                }
+            }
+            else if(weight == 0)
+            {
+                if(edge_exist_flag == 1)
+                {
+                    exist_vertex_source.remove_edge(target);
+               
+                    checkRep();
+                    
+                    return edge_exist_weight;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        
+        return -1;
     }
     
     @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        
+        int exist_flag = 0;
+        
+        Vertex exist_vertex = null;
+        
+        for(Vertex v : vertices)
+        {
+            if(v.getVertex().equals(vertex))
+            {
+                exist_vertex = v;
+                
+                exist_flag = 1;
+                
+                break;
+            }
+        }
+        
+        if(exist_flag == 0)
+        {
+            return false;
+        }
+        else
+        {
+            vertices.remove(exist_vertex);
+            
+            for(Vertex v : vertices)
+            {
+                v.remove_edge(vertex);
+            }
+            
+            checkRep();
+            
+            return true;
+        }
+        
     }
     
     @Override public Set<String> vertices() {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        
+        Set<String> return_vertex = new HashSet<>();
+        
+        for(Vertex v : vertices)
+        {
+            return_vertex.add(v.getVertex());
+        }
+        
+        checkRep();
+        
+        return return_vertex;
     }
     
     @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        
+        Map<String, Integer> return_source = new HashMap<>();
+        
+        for(Vertex v : vertices)
+        {
+            if(v.getEdge().keySet().contains(target))
+            {
+                return_source.put(v.getVertex(), v.getEdge().get(target));
+            }
+        }
+        
+        checkRep();
+        
+        return return_source;
+        
     }
     
     @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
+        //throw new RuntimeException("not implemented");
+        
+        Map<String, Integer> return_target = null;
+        
+        for(Vertex v : vertices)
+        {
+            if(v.getVertex().equals(source))
+            {
+                return_target = v.getEdge();
+                
+                break;
+            } 
+        }
+        
+        checkRep();
+        
+        return return_target;
     }
     
     // TODO toString()
@@ -57,7 +307,7 @@ public class ConcreteVerticesGraph implements Graph<String> {
 }
 
 /**
- * TODO specification
+ * this mutable data type represent a vertex(include all the edge from this vertex)
  * Mutable.
  * This class is internal to the rep of ConcreteVerticesGraph.
  * 
@@ -66,20 +316,107 @@ public class ConcreteVerticesGraph implements Graph<String> {
  */
 class Vertex {
     
-    // TODO fields
+    // fields
+    private final String vertex;
+    private final Map<String, Integer> edges = new HashMap<>();
     
     // Abstraction function:
-    //   TODO
+    // represent a vertex include all edges from this vertex
     // Representation invariant:
-    //   TODO
+    // edges is no duplicate and weight is bigger than zero
     // Safety from rep exposure:
-    //   TODO
+    // all fields is private and vertex is immutable , edges is mutable but make defensive copies in getEdge()
     
-    // TODO constructor
+    /**
+     * create vertex with empty edge set(which source is this vertex)
+     * @param vertex vertex label
+     */
+    public Vertex(String vertex)
+    {
+        this.vertex = vertex;
+        
+        checkRep();
+    }
     
-    // TODO checkRep
+    /**
+     * check  representation invariant of the Vertex is true 
+     */
+    private void checkRep()
+    {
+        assert vertex != null;
+        for(Map.Entry<String, Integer> entry : edges.entrySet())
+        {
+            assert entry.getKey() != null;
+            assert entry.getValue() > 0;
+            
+            assert entry.getValue() != null;
+        }
+    }
     
-    // TODO methods
+    // methods
+    
+    /**
+     * add edge(add new edge which source is this vertex)
+     * @param target new edge's target (require target is not in this vertex's edge set)
+     * @param weight new edge's weight (require weight is bigger than zero)
+     */
+    public void add_edge(String target, Integer weight)
+    {
+        edges.put(target, weight);
+        
+        checkRep();
+    }
+    
+    
+    /**
+     * set edge(set new weight of edge which source is this vertex and exits in this vertex's edge set )
+     * @param target the target of edge need to be set(require in this vertex's edge set)
+     * @param weight the weight of edge need to be set(require weight is bigger than zero)
+     */
+    public void set_edge(String target, Integer weight)
+    {
+        edges.replace(target, weight);
+        
+        checkRep();
+    }
+    
+    /**
+     * remove edge from this vertex which target is special vertex 
+     * @param vertex the target of edge need to be remove(require vertex is in this vertex edge set)
+     */
+    public void remove_edge(String vertex)
+    {
+        edges.remove(vertex);
+        
+        checkRep();
+    }
+    
+    
+    /**
+     * get this vertex label
+     * @return return this vertex label
+     */
+    public String getVertex()
+    {
+        return vertex;
+    }
+    
+    /**
+     * get the edge set of this vertex
+     * @return edge map with key is String which represent target and value is Integer which represent weight
+     */
+    public Map<String, Integer> getEdge()
+    {
+        Map<String, Integer> return_edges = new HashMap<>();
+        
+        for(Map.Entry<String, Integer> entry : edges.entrySet())
+        {
+            return_edges.put(entry.getKey(), entry.getValue());
+        }
+        
+        return return_edges;
+    }
+    
     
     // TODO toString()
     
--- test/graph/ConcreteVerticesGraphTest.java
+++ test/graph/ConcreteVerticesGraphTest.java
@@ -4,6 +4,7 @@
 package graph;
 
 import static org.junit.Assert.*;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -38,8 +39,61 @@ public class ConcreteVerticesGraphTest extends GraphInstanceTest {
      */
     
     // Testing strategy for Vertex
-    //   TODO
-    
+    // Vertex():
+    // just vertex without edge
+    // add_edge():
+    // add edge and weight is legal
+    // set_edge():
+    // set edge and weight is legal  and edge is in this vertex 
+    // remove_edge():
+    // remove edge which target is in this vertex 
+    // getVertex():
+    // vertex has edge 
+    // getEdge():
+    // has zero or one more edge
+    //
     // TODO tests for operations of Vertex
     
+    @Test
+    public void testVertex()
+    {
+        Vertex v = new Vertex("Num1");
+        
+        assertEquals("expect vertex is Num1", "Num1", v.getVertex());
+    }
+    
+    @Test
+    public void testAddAndSetAndRemove()
+    {
+        Vertex v = new Vertex("Num1");
+        v.add_edge("Num2", Integer.valueOf(1));
+        
+        Map<String, Integer> mp1 = v.getEdge();
+        assertEquals("expected edge size is 1", 1, mp1.size());
+        assertEquals("expected target is Num2 and weight is 1",Integer.valueOf(1), mp1.get("Num2"));
+        
+        
+        v.add_edge("Num3", Integer.valueOf(2));
+        
+        Map<String, Integer> mp2 = v.getEdge();
+        assertEquals("expected edge size is 2", 2, mp2.size());
+        assertEquals("expected target is Num3 and weight is 2",Integer.valueOf(2), mp2.get("Num3"));
+        
+        v.set_edge("Num2", 3);
+        
+        Map<String, Integer> mp3 = v.getEdge();
+        assertEquals("expected edge size is 2", 2, mp3.size());
+        assertEquals("expected target is Num2 and weight is 3",Integer.valueOf(3), mp3.get("Num2"));
+        
+        v.remove_edge("Num2");
+        
+        Map<String, Integer> mp4 = v.getEdge();
+        assertEquals("expected edge size is 1", 1, mp4.size());
+        assertEquals("expected target is Num3 and weight is 2",Integer.valueOf(2), mp4.get("Num3"));
+        
+        v.remove_edge("Num3");
+        
+        Map<String, Integer> mp5 = v.getEdge();
+        assertEquals("expected edge size is 0", 0, mp5.size());
+    }
 }
